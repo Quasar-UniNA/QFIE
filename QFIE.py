db6738a2ac3bf0907302b08db7aedcaf2dd04d71
quasarUnina <quasar@quasar.unina.it>
2022-09-08 11:08:16 +0200

    Update QFIE.py

--- QFIE.py
+++ QFIE.py
@@ -13,6 +13,16 @@ from qiskit.visualization import plot_histogram
 
 
 class QFIE:
+    '''
+    Class implementing the Quantum Fuzzy Inference Engine proposed in:
+
+    G. Acampora, R. Schiattarella and A. Vitiello, "On the Implementation of Fuzzy Inference Engines on Quantum Computers,"
+    in IEEE Transactions on Fuzzy Systems, 2022, doi: 10.1109/TFUZZ.2022.3202348.
+    ________________
+
+    
+
+    '''
     def __init__(self):
         self.input_ranges = {}
         self.output_range = {}
