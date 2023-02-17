e7843babfccb26bd23952bbc511c8f2c4c2a1d61
Rschiattarella <roberto8schiattarella@gmail.com>
2023-02-17 17:03:22 +0100

    Cleaning D-NISQ Code

--- src/QFIE/FuzzyEngines.py
+++ src/QFIE/FuzzyEngines.py
@@ -187,38 +187,58 @@ class QuantumFuzzyEngine:
         Returns:
             None
         """
+        # Print Crisp Inputs
+        if self.verbose:
+            print(input_values)
+
+        # FUZZIFICATION
+        fuzzyfied_values = {}
+        norm_values = {}
+        for var_name in list(input_values.keys()):
+            fuzzyfied_values[var_name] = [
+                fuzz.interp_membership(
+                    self.input_ranges[var_name], i, input_values[var_name]
+                )
+                for i in self.input_fuzzysets[var_name]
+            ]
+        if self.verbose:
+            print("Input values ", fuzzyfied_values)
+
+        # CIRCUIT SETUPS
+        # Not Distributed QFIE
         if not distributed:
             self.qc['full_circuit'] = QFS.generate_circuit(list(self.input_partitions.values()))
             self.qc['full_circuit'] = QFS.output_register(self.qc['full_circuit'], list(self.output_partition.values())[0])
-            if self.verbose:
-                print(input_values)
-            fuzzyfied_values = {}
-            norm_values = {}
-            for var_name in list(input_values.keys()):
-                fuzzyfied_values[var_name] = [
-                    fuzz.interp_membership(
-                        self.input_ranges[var_name], i, input_values[var_name]
-                    )
-                    for i in self.input_fuzzysets[var_name]
-                ]
-                # norm_values[var_name] = [self.truncate(float(i)/sum(fuzzyfied_values[var_name]), 3) for i in fuzzyfied_values[var_name]]
-            if self.verbose:
-                print("Input values ", fuzzyfied_values)
-            initial_state = {}
-            for var_name in list(input_values.keys()):
-                initial_state[var_name] = [
-                    math.sqrt(fuzzyfied_values[var_name][i])
-                    for i in range(len(fuzzyfied_values[var_name]))
-                ]
-                required_len = QFS.select_qreg_by_name(self.qc['full_circuit'], var_name).size
-                while len(initial_state[var_name]) != 2 ** required_len:
-                    initial_state[var_name].append(0)
-                initial_state[var_name][-1] = math.sqrt(1 - sum(fuzzyfied_values[var_name]))
-                # print(initial_state)
-                self.qc['full_circuit'].initialize(
-                    initial_state[var_name], QFS.select_qreg_by_name(self.qc['full_circuit'], var_name)
+        # Distributed QFIE
+        else:
+            self.out_register_name = []
+            # Use output linguistic terms as labels (keys) to identify the corresponding distributed circuits
+            qc_labels = self.output_partition[list(self.output_fuzzyset.keys())[0]].sets
+            for label in qc_labels:
+                # Create a quantum circuit corresponding to each label
+                self.qc[label] = QFS.generate_circuit(list(self.input_partitions.values()))
+                self.qc[label] = QFS.output_single_qubit_register(self.qc[label], label)
+                # Create a subset of rules corresponding to each label
+                self.rule_subsets[label] = self.filter_rules(self.rules, label)
+
+        # COMPUTING AMPLITUDES FROM FUZZIFIED VALUES
+        initial_state = {}
+        for var_name in list(input_values.keys()):
+            initial_state[var_name] = [
+                math.sqrt(fuzzyfied_values[var_name][i])
+                for i in range(len(fuzzyfied_values[var_name]))
+            ]
+            required_len = QFS.select_qreg_by_name(list(self.qc.values())[0], var_name).size
+            while len(initial_state[var_name]) != 2 ** required_len:
+                initial_state[var_name].append(0)
+            initial_state[var_name][-1] = math.sqrt(1 - sum(fuzzyfied_values[var_name]))
+            for circ in list(self.qc.values()):
+                circ.initialize(
+                    initial_state[var_name], QFS.select_qreg_by_name(circ, var_name)
                 )
 
+        # BUILDING ORACLES
+        if not distributed:
             for rule in self.rules:
                 QFS.convert_rule(
                     qc=self.qc['full_circuit'],
@@ -238,59 +258,12 @@ class QuantumFuzzyEngine:
                 else:
                     self.qc['full_circuit'].draw("mpl").show()
         else:
-
-            # Distributed QFIE
-
             self.out_register_name = []
-
             # Use output linguistic terms as labels (keys) to identify the corresponding distributed circuits
             qc_labels = self.output_partition[list(self.output_fuzzyset.keys())[0]].sets
-
             for label in qc_labels:
-
-                # Create a quantum circuit corresponding to each label
-                self.qc[label] = QFS.generate_circuit(list(self.input_partitions.values()))
-                self.qc[label] = QFS.output_single_qubit_register(self.qc[label], label)
-
-                # Create a subset of rules corresponding to each label
-                self.rule_subsets[label] = self.filter_rules(self.rules, label)
-
-                # TODO
-                # if self.verbose:
-                # print(input_values)
-
                 modified_output_partition = deepcopy(list(self.output_partition.values())[0])
                 modified_output_partition.sets = [label]
-
-                fuzzyfied_values = {}
-                norm_values = {}
-                for var_name in list(input_values.keys()):
-                    fuzzyfied_values[var_name] = [
-                        fuzz.interp_membership(
-                            self.input_ranges[var_name], i, input_values[var_name]
-                        )
-                        for i in self.input_fuzzysets[var_name]
-                    ]
-
-                # TODO
-                # if self.verbose:
-                # print("Input values ", fuzzyfied_values)
-
-                initial_state = {}
-                for var_name in list(input_values.keys()):
-                    initial_state[var_name] = [
-                        math.sqrt(fuzzyfied_values[var_name][i])
-                        for i in range(len(fuzzyfied_values[var_name]))
-                    ]
-                    required_len = QFS.select_qreg_by_name(self.qc[label], var_name).size
-                    while len(initial_state[var_name]) != 2 ** required_len:
-                        initial_state[var_name].append(0)
-                    initial_state[var_name][-1] = math.sqrt(1 - sum(fuzzyfied_values[var_name]))
-                    # print(initial_state)
-
-                    self.qc[label].initialize(
-                        initial_state[var_name], QFS.select_qreg_by_name(self.qc[label], var_name)
-                    )
                 for rule in self.rule_subsets[label]:
                     QFS.convert_rule(
                         qc=self.qc[label],
@@ -299,7 +272,6 @@ class QuantumFuzzyEngine:
                         output_partition=modified_output_partition,
                     )
                     self.qc[label].barrier()
-
                 self.out_register_name.append(list(self.output_fuzzyset.keys())[0] + " " + label)
                 out = ClassicalRegister(1)
                 self.qc[label].add_register(out)
@@ -334,18 +306,13 @@ class QuantumFuzzyEngine:
                 backend.set_options(device='GPU')
             except: print('Not possible use GPU for this quantum backend or your device is not equipped with GPUs')
 
-        if 'transpile_info' in kwargs:
-            if kwargs['transpile_info'] == True:
+        # COMPUTE NOT DISTRIBUTED ALGORITHM
+        if len(self.qc) == 1:
+            if 'transpile_info' in kwargs and kwargs['transpile_info'] == True:
                 self.transpiled_qc = transpile(self.qc['full_circuit'], backend, optimization_level=3)
                 print('transpiled depth ', self.transpiled_qc.depth())
                 print('CNOTs number ', self.transpiled_qc.count_ops()['cx'])
-
-        if len(self.qc) == 1:
-            # Normal version
-
-            if 'transpile_info' in kwargs:
-                if kwargs['transpile_info'] == True:
-                    job = execute(self.transpiled_qc, backend, shots=n_shots)
+                job = execute(self.transpiled_qc, backend, shots=n_shots)
             else:
                 job = execute(self.qc['full_circuit'], backend, shots=n_shots)
             if plot_histo:
@@ -359,53 +326,19 @@ class QuantumFuzzyEngine:
             output_dict = {
                 i: [] for i in self.output_partition[self.out_register_name].sets
             }
-            counter = 0
-            for set in list(output_dict.keys()):
-                counter = counter + 1
-                for i in range(self.n_q):
-                    if i == self.n_q - counter:
-                        output_dict[set].append("1")
-                    else:
-                        output_dict[set].append("0")
-                output_dict[set] = "".join(output_dict[set])
-
-            memberships = {}
-            for state in list(output_dict.values()):
-                if state in list(normalized_counts.keys()):
-                    memberships[state] = normalized_counts[state]
-                else:
-                    memberships[state] = 0
-
-            norm_memberships = memberships
-            if self.verbose:
-                print("Output Counts", memberships)
-            activation = {}
-            set_number = 0
-            for set in list(output_dict.keys()):
-                activation[set] = np.fmin(
-                    norm_memberships[output_dict[set]],
-                    self.output_fuzzyset[self.out_register_name][set_number],
-                )
-                set_number = set_number + 1
-
-            activation_values = list(activation.values())[::-1]
-            aggregated = np.zeros(self.output_fuzzyset[self.out_register_name][0].shape)
-            for i in range(len(activation_values)):
-                aggregated = np.fmax(aggregated, activation_values[i])
-
-            return (
-                fuzz.defuzz(
-                    self.output_range[self.out_register_name], aggregated, "centroid"
-                ),
-                activation_values,
-            )
-
+        # COMPUTE DISTRIBUTED ALGORITHM
         else:
             # Distributed version
             qc_labels = self.output_partition[list(self.output_fuzzyset.keys())[0]].sets
             subcounts = {}
             for label in qc_labels:
-                job = execute(self.qc[label], backend, shots=n_shots)
+                if 'transpile_info' in kwargs and kwargs['transpile_info'] == True:
+                    self.transpiled_qc = transpile(self.qc[label], backend, optimization_level=3)
+                    print('transpiled depth qc ' + str(label), self.transpiled_qc.depth())
+                    print('CNOTs number qc ' + str(label), self.transpiled_qc.count_ops()['cx'])
+                    job = execute(self.transpiled_qc, backend, shots=n_shots)
+                else:
+                    job = execute(self.qc[label], backend, shots=n_shots)
                 result = job.result()
                 subcounts[label] = result.get_counts()
             self.counts_ = QFS.merge_subcounts(subcounts, self.output_partition[list(self.output_fuzzyset.keys())[0]])
@@ -415,51 +348,52 @@ class QuantumFuzzyEngine:
                 ).show()
             self.n_q = len(self.output_fuzzyset[list(self.output_fuzzyset.keys())[0]])
             counts = self.counts_evaluator(n_qubits=self.n_q, counts=self.counts_)
-            # normalized_counts = {k: v / total for total in (sum(counts.values()),) for k, v in counts.items()}
             normalized_counts = counts
             output_dict = {
                 i: [] for i in self.output_partition[list(self.output_fuzzyset.keys())[0]].sets
             }
-            counter = 0
-            for set in list(output_dict.keys()):
-                counter = counter + 1
-                for i in range(self.n_q):
-                    if i == self.n_q - counter:
-                        output_dict[set].append("1")
-                    else:
-                        output_dict[set].append("0")
-                output_dict[set] = "".join(output_dict[set])
 
-            memberships = {}
-            for state in list(output_dict.values()):
-                if state in list(normalized_counts.keys()):
-                    memberships[state] = normalized_counts[state]
+        counter = 0
+        for set in list(output_dict.keys()):
+            counter = counter + 1
+            for i in range(self.n_q):
+                if i == self.n_q - counter:
+                    output_dict[set].append("1")
                 else:
-                    memberships[state] = 0
+                    output_dict[set].append("0")
+            output_dict[set] = "".join(output_dict[set])
 
-            norm_memberships = memberships
-            if self.verbose:
-                print("Output Counts", memberships)
-            activation = {}
-            set_number = 0
-            for set in list(output_dict.keys()):
-                activation[set] = np.fmin(
-                    norm_memberships[output_dict[set]],
-                    self.output_fuzzyset[list(self.output_fuzzyset.keys())[0]][set_number],
-                )
-                set_number = set_number + 1
+        memberships = {}
+        for state in list(output_dict.values()):
+            if state in list(normalized_counts.keys()):
+                memberships[state] = normalized_counts[state]
+            else:
+                memberships[state] = 0
 
-            activation_values = list(activation.values())[::-1]
-            aggregated = np.zeros(self.output_fuzzyset[list(self.output_fuzzyset.keys())[0]][0].shape)
-            for i in range(len(activation_values)):
-                aggregated = np.fmax(aggregated, activation_values[i])
-
-            return (
-                fuzz.defuzz(
-                    self.output_range[list(self.output_fuzzyset.keys())[0]], aggregated, "centroid"
-                ),
-                activation_values,
+        # DEFUZZIFICATION
+        norm_memberships = memberships
+        if self.verbose:
+            print("Output Counts", memberships)
+        activation = {}
+        set_number = 0
+        for set in list(output_dict.keys()):
+            activation[set] = np.fmin(
+                norm_memberships[output_dict[set]],
+                self.output_fuzzyset[list(self.output_fuzzyset.keys())[0]][set_number],
             )
+            set_number = set_number + 1
+
+        activation_values = list(activation.values())[::-1]
+        aggregated = np.zeros(self.output_fuzzyset[list(self.output_fuzzyset.keys())[0]][0].shape)
+        for i in range(len(activation_values)):
+            aggregated = np.fmax(aggregated, activation_values[i])
+
+        return (
+            fuzz.defuzz(
+                self.output_range[list(self.output_fuzzyset.keys())[0]], aggregated, "centroid"
+            ),
+            activation_values,
+        )
 
 
 
